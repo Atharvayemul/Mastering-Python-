fa7278c40fa7d73e6b8c63bc942fdac24cc443e6
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-26 18:36:36 +0530

    Updated

---
+++ Python Basics/Arithmetic.py
@@ -0,0 +1,23 @@
+x = 10 + 3
+
+x = 10 - 3
+
+x = 10 * 3
+
+x = 10 / 3
+
+x = 10 // 3
+
+x = 10 % 3
+
+x = 10 ** 3
+
+x = x + 1
+
+x += 1
+
+# x++
+# x--
+# Is Not Supported in Python
+
+print(x)
---
+++ Python Basics/Working_with_number.py
@@ -0,0 +1,6 @@
+
+# Like Another langauges constant is not provided in python
+
+PI = 3.14
+
+print(round(PI))
