7385a8481e8bf1c3ed28c0717b3abce16044de3f
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-26 18:43:59 +0530

    Type_converesion

---
+++ Python Basics/Type_Conversion.py
@@ -0,0 +1,18 @@
+
+# There is no implicit type conversino in python
+# So called Strongly Typed Language
+
+x = input("x:")
+
+print(int(x))
+print(float(x))
+print(bool(x))
+
+# Falsy Values in bool conversion
+# ""
+# 0
+# []
+# null (null)
+
+
+# Anything else Above is considered as True while conversion to Bool
