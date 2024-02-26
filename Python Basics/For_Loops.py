a355efd0bcf241d5f89348244b3f6d78e2d42cf6
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-26 19:02:54 +0530

    Loops

---
+++ Python Basics/For_Loops.py
@@ -0,0 +1,14 @@
+
+for x in "Python":
+    print(x)
+
+
+for x in ['a', 'b', 'c']:
+    print(x)
+
+
+for x in range(2, 5):  # 5  is Excluded
+    print(x)
+
+for y in range(0, 10, 2):
+    print(y)
