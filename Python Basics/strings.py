e6d0796350ff4571167ad5bb61f12bf91edb6ba9
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-26 18:04:21 +0530

    Strings

---
+++ Python Basics/strings.py
@@ -0,0 +1,14 @@
+
+course = "Python Programming"
+
+print(len(course))
+
+print(course[0])
+print(course[-1])
+
+# the end is excluded in this sliing
+print(course[0:3])
+
+# This returns the entire string
+print(course[0:])
+print(course[:])
