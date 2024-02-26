5d1d1c9edc3c029c4badbbc593418f85c3f11c44
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-26 18:48:51 +0530

    Condition

---
+++ Python Basics/Condition.py
@@ -0,0 +1,12 @@
+age = 22
+
+if age >= 18:
+    print("Adult")
+elif age >= 13:
+    print("Teen")
+else:
+    print("Child")
+
+print("All done")
+
+# We cacnnot have empty if block if we have empty then we have to use pass keyword
