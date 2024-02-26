b25a3dd86b830f4c243e4ff74f8525c7a8dc578b
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-26 19:35:44 +0530

    functions

---
+++ Python Basics/Functions.py
@@ -0,0 +1,9 @@
+# Python Function always returns None
+# In Python we can also return Multiple values
+
+
+def increment(number, by):
+    return (number, number + by)
+
+
+print(increment(number=2, by=3))
