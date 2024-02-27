4a85a4fb4107988a16250489b6f8863e63b7b33e
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-27 19:11:05 +0530

    Looping over list

---
+++ Data Structures/4_Looping_over_list.py
@@ -0,0 +1,5 @@
+letters = ["a", "b", "c"]
+
+# This enumeration returns the object where each object contains a tuple containing the index and the value
+for index, letter in enumerate(letters):
+    print(index, letter)
