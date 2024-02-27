081815c14ba27d67d32b0eab196cfdd5cedd1c5d
Atharva Yemul <atharvayemul04@gmail.com>
2024-02-27 18:56:38 +0530

    Lists

---
+++ Data Structures/1_Lists.py
@@ -0,0 +1,15 @@
+letters = ["a", "b", "c"]
+
+matrix = [[0, 1], [2, 3]]
+
+zeros = [0] * 100
+
+combined = zeros + letters
+
+numbers = list(range(20))
+print(numbers)
+
+chars = list("Hello World")
+
+print(chars)
+print(len(chars))
